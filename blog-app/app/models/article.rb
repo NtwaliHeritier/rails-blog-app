05d27faf6bf8473ef566b0797407d6d9eb865b70
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-13 15:51:05 +0200

    add tag model and controller

--- blog-app/app/models/article.rb
+++ blog-app/app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ApplicationRecord
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+    def tag_list
+        tags.collect do |tag|
+            tag.name
+        end.join(",")
+    end
+
+    def tag_list=(tag_strings)
+        tag_names=tag_strings.split(",").collect{|s| s.strip.downcase}.uniq
+        new_or_found_tags=tag_names.collect{|s| Tag.find_or_create_by(name: s)}
+        self.tags=new_or_found_tags
+    end
 end
